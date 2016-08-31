d01a89d77d3865c9849a7aa03247123f991dc7d3
faizal-mubarok <faizal-mubarok@users.noreply.github.com>
2016-08-31 17:07:02 +0700

    Add files via upload

---
+++ TugasBintang1.java
@@ -0,0 +1,14 @@
+public class TugasBintang1{
+	public static void main (String[] args) { 
+		int b;
+		int k;
+		for (b=1; b<=20; b++)
+		{
+			for (k=1; k<=1; k++)
+			{
+			System.out.print("*");
+			}
+				System.out.println();
+		}
+}
+}
